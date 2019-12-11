ab6132cb018a0ca9dd2bfa78ce8bcac73c78a118
Luis Antonio Quintero Robledo <16001555@alumnos.utleon.edu.mx>
2019-12-11 17:31:26 -0600

    Conexion con local

--- ProyectoFinal/ProyectoFinal/AccesoDatos/ConexionSQL.cs
+++ ProyectoFinal/ProyectoFinal/AccesoDatos/ConexionSQL.cs
@@ -36,8 +36,8 @@ namespace ProyectoFinal.AccesoDatos
         /// </remarks>
         public SqlConnection GetConnection() {
             DBUser user = GetDBUser();
-            //string conString = @"DATA SOURCE = " + Server + " ; USER ID = " + user.Username + "; PASSWORD = " + user.Password + "; INITIAL CATALOG = " + DBName + "; TIMEOUT = 10;";
-            string conString = @"DATA SOURCE = DESKTOP-PQOR5LH\SQLEXPRESS ; USER ID = " + user.Username + "; PASSWORD = " + user.Password + "; INITIAL CATALOG = " + DBName + "; TIMEOUT = 10;";
+            string conString = @"DATA SOURCE = " + Server + " ; USER ID = " + user.Username + "; PASSWORD = " + user.Password + "; INITIAL CATALOG = " + DBName + "; TIMEOUT = 10;";
+            //string conString = @"DATA SOURCE = DESKTOP-PQOR5LH\SQLEXPRESS ; USER ID = " + user.Username + "; PASSWORD = " + user.Password + "; INITIAL CATALOG = " + DBName + "; TIMEOUT = 10;";
             
             SqlConnection conn = new SqlConnection(conString);
 
